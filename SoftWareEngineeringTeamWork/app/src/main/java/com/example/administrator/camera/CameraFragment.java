efa49a5258bc4e281a98827f3742a10a916539c8
ChenBattle <694486601@qq.com>
2018-11-24 22:31:05 +0800

    上传测试bug_忽略此版本

--- SoftWareEngineeringTeamWork/app/src/main/java/com/example/administrator/camera/CameraFragment.java
+++ SoftWareEngineeringTeamWork/app/src/main/java/com/example/administrator/camera/CameraFragment.java
@@ -40,6 +40,7 @@ import com.bumptech.glide.request.target.SimpleTarget;
 import com.bumptech.glide.request.target.SizeReadyCallback;
 import com.bumptech.glide.request.target.Target;
 import com.bumptech.glide.request.transition.Transition;
+import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 
 import java.io.ByteArrayOutputStream;
@@ -136,7 +137,8 @@ public class CameraFragment extends Fragment {
 //                downloadPic("QQ.png");
 //                downloadPic("wechat.png");
 //                downloadPic("pineapple.png");
-                downloadPic("test.jpg");
+//                downloadPic("test.jpg");
+                uploadPic("tem_picture.jpg");
 
                 popupWindow.dismiss();
             }
@@ -205,6 +207,43 @@ public class CameraFragment extends Fragment {
     }
 
     private final OkHttpClient client = new OkHttpClient();
+    private static final MediaType MEDIA_TYPE_PNG = MediaType.parse("image/png");
+
+    private void uploadPic(final String fileName)
+    {
+//        // mImgUrls为存放图片的url集合
+//        MultipartBody.Builder builder = new MultipartBody.Builder().setType(MultipartBody.FORM);
+//        for (int i = 0; i <mImgUrls.size() ; i++) {
+//            File f=new File(mImgUrls.get(i));
+//            if (f!=null) {
+//                builder.addFormDataPart("img", f.getName(), RequestBody.create(MEDIA_TYPE_PNG, f));
+//            }
+//        }
+//
+//        MultipartBody requestBody = builder.build();
+//        //构建请求
+//        Request request = new Request.Builder()
+//                .url(Constant.BASE_URL)//地址
+//                .post(requestBody)//添加请求体
+//                .build();
+//
+//        client.newCall(request).enqueue(new Callback() {
+//            @Override
+//            public void onFailure(Call call, IOException e) {
+//
+//                System.out.println("上传失败:e.getLocalizedMessage() = " + e.getLocalizedMessage());
+//            }
+//
+//            @Override
+//            public void onResponse(Call call, Response response) throws IOException {
+//
+//                System.out.println("上传照片成功：response = " + response.body().string());
+//                ToastCustom.makeText(PictureListActivity.this, "上传成功", Toast.LENGTH_LONG).show();
+//
+//
+//            }
+//        });
+    }
 
     private void downloadPic(final String fileName) {
         new Thread(){
