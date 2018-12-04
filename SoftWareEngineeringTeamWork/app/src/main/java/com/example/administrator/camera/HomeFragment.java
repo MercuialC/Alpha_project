86ea75126dabc09f4f3e49841826b722b413c0c9
ChenBattle <694486601@qq.com>
2018-12-04 21:05:50 +0800

    map_test

--- SoftWareEngineeringTeamWork/app/src/main/java/com/example/administrator/camera/HomeFragment.java
+++ SoftWareEngineeringTeamWork/app/src/main/java/com/example/administrator/camera/HomeFragment.java
@@ -6,13 +6,16 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
+    private Button btn_map;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -25,6 +28,21 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        btn_map = view.findViewById(R.id.btn_map);
+        btn_map.setOnClickListener(this);
+
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btn_map:
+                Toast.makeText(getActivity(), "点击", Toast.LENGTH_SHORT).show();
+
+                break;
+        }
     }
 }
