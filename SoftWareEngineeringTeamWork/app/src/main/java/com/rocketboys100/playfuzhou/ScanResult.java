aa4d38ac683b0da59e4adf8f01ab0b77a80250e6
MercurialC <946196959@qq.com>
2018-12-21 00:20:49 +0800

    back3
    back3

--- SoftWareEngineeringTeamWork/app/src/main/java/com/rocketboys100/playfuzhou/ScanResult.java
+++ SoftWareEngineeringTeamWork/app/src/main/java/com/rocketboys100/playfuzhou/ScanResult.java
@@ -1,20 +1,26 @@
 package com.rocketboys100.playfuzhou;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class ScanResult extends AppCompatActivity {
 
 
-
+    private Button bBack3=findViewById(R.id.btn_back3);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_result);
         getSupportActionBar().hide();
-
-
+        bBack3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
-
 }
