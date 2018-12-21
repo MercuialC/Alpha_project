90b9c01d0618cc0743c8fc1443b1ff61bda94fbd
MercurialC <946196959@qq.com>
2018-12-21 18:04:05 +0800

    弹幕修改
    弹幕修改

--- SoftWareEngineeringTeamWork/app/src/main/java/com/rocketboys100/playfuzhou/ScanResult.java
+++ SoftWareEngineeringTeamWork/app/src/main/java/com/rocketboys100/playfuzhou/ScanResult.java
@@ -28,8 +28,8 @@ public class ScanResult extends AppCompatActivity {
         private TranslateAnimation translateAnimation;
         //我用的是TextView.这个看自己喜好。这里的boolean量是我用来实现两种弹幕特效
         public SendDanmu(TextView textView, RelativeLayout relativeLayout){
-            int length=relativeLayout.getBottom()-relativeLayout.getTop();      //获取relativelayout的长度
-            int y=relativeLayout.getTop()+(int)(Math.random()*length);       //设置弹幕随机产生的y坐标
+            int length=relativeLayout.getBottom()-relativeLayout.getTop()-100;      //获取relativelayout的长度
+            int y=relativeLayout.getTop()+(int)(Math.random()*length)+100;       //设置弹幕随机产生的y坐标
 //          //天女散花型
 //                translateAnimation=new TranslateAnimation(relativeLayout.getLeft(),relativeLayout.getRight(),
 //                        relativeLayout.getRight(),y);
@@ -66,11 +66,11 @@ public class ScanResult extends AppCompatActivity {
     }
 
     public void adddamu(boolean text) {
-        String[] str = {"菠萝牛逼","菠萝马中马","菠萝带你timi带你飞","我是菠萝！"};
+        String[] str = {"菠萝牛逼菠萝牛逼菠萝牛逼菠萝牛逼菠萝牛逼菠萝牛逼菠萝牛逼菠萝牛逼菠萝牛逼菠萝牛逼菠萝牛逼菠萝牛逼","菠萝马中马","菠萝带你timi带你飞","我是菠萝！"};
         int[] color = {R.color.colorAccent,R.color.colorPrimary,R.color.colorPrimaryDark};//{R.color.red, R.color.blue, R.color.white,R.color.yellow};
 
         TextView tv = new TextView(this);
-        tv.setTextSize(20);
+        tv.setTextSize(15);
         tv.setTextColor(Color.rgb(random.nextInt(256),random.nextInt(256), random.nextInt(256)));
         tv.setText(str[(int) (Math.random() * str.length)]);
 
@@ -88,12 +88,12 @@ public class ScanResult extends AppCompatActivity {
                 handler.sendMessage(message);
             }
         };
-        timer.schedule(task, new Date(), 200);
+        timer.schedule(task, new Date(), 700);
     }
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            if (i == 4) {
+            if (i == 3) {
                 //清除产生的控件
                 root.removeAllViews();
                 i = 0;
